7d81ac93b9ee4fcbee5dfc08825529943bd2abbb
LiamSalass <liamsalass@yahoo.com>
2023-05-09 12:44:23 -0400

    Rearrange files

--- cpp_models/rcvpose_cpp/train.h
+++ cpp_models/rcvpose_cpp/train.h
@@ -1,1 +1,10 @@
 #pragma once
+
+// Path: train.h
+// Main file for training pipeline
+
+#include "models/denseFCNResNet152.h
+#include "models/denseFCNResNet50.h"
+#include "AccumulatorSpace.h"
+#include "utils.h"
+

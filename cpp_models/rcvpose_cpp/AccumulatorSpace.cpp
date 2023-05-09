f88857cf1c3fa260a2fdfee9104655070110e29b
LiamSalass <liamsalass@yahoo.com>
2023-05-09 16:54:48 -0400

    Added open3d to codebase

--- cpp_models/rcvpose_cpp/AccumulatorSpace.cpp
+++ cpp_models/rcvpose_cpp/AccumulatorSpace.cpp
@@ -13,29 +13,38 @@ Matrix dot(Matrix A, Matrix B) {
     }
     return C;
 }
-
+//TODO: Check implementation
 std::vector<Matrix, Matrix> project(Matrix xyz, Matrix K, Matrix RT) {
     int N = xyz.size();
+
     Matrix xyz_T(3, std::vector<float>(N, 0));
+
     Matrix RT_T = dot(K, RT);
+
     for (int i = 0; i < N; i++) {
         for (int j = 0; j < 3; j++) {
             xyz_T[j][i] = xyz[i][j];
         }
     }
+
     Matrix actual_xyz_T = dot(RT_T, xyz_T);
+
     Matrix actual_xyz(N, std::vector<float>(3, 0));
     for (int i = 0; i < N; i++) {
         for (int j = 0; j < 3; j++) {
             actual_xyz[i][j] = actual_xyz_T[j][i];
         }
     }
+
     Matrix xyz_K_T = dot(K, actual_xyz_T);
+
     Matrix xy(N, std::vector<float>(2, 0));
+
     for (int i = 0; i < N; i++) {
         for (int j = 0; j < 2; j++) {
             xy[i][j] = xyz_K_T[j][i] / xyz_K_T[2][i];
         }
     }
+
     return { xy, actual_xyz };
 }

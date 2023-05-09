27d0864d1e416139e26ae561eaa2f2b7cba12547
LiamSalass <liamsalass@yahoo.com>
2023-05-09 13:41:44 -0400

    optimized options struct

--- cpp_models/rcvpose_cpp/main.h
+++ cpp_models/rcvpose_cpp/main.h
@@ -4,17 +4,17 @@
 #include "train.h"
 
 struct Options {
-    std::string mode = "train";
-    int gpu_id = -1;
-    std::string dname = "lm";
-    std::string root_dataset = "./datasets/LINEMOD";
     bool resume_train = false;
-    std::string optim = "Adam";
-    int batch_size = 4;
-    std::string class_name = "ape";
-    float initial_lr = 1e-4;
-    int kpt_num = 1;
-    std::string model_dir = "ckpts/";
     bool demo_mode = false;
     bool test_occ = false;
+    int gpu_id = -1;
+    int batch_size = 4;
+    int kpt_num = 1;
+    float initial_lr = 1e-4;
+    std::string mode = "train";
+    std::string dname = "lm";
+    std::string root_dataset = "./datasets/LINEMOD";
+    std::string optim = "Adam";
+    std::string class_name = "ape";
+    std::string model_dir = "ckpts/";
 };

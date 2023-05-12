dfe360ec746640d42451b2fa1bfd68ecc556f38e
LiamSalass <liamsalass@yahoo.com>
2023-05-12 00:09:03 -0400

    Python linking error

--- cpp_models/embedded_testing/test.cpp
+++ cpp_models/embedded_testing/test.cpp
@@ -11,9 +11,13 @@ namespace py = pybind11;
 int main(int argc, char* argv[])
 {
 	std::cout << "[cpp] Hello World!" << std::endl;
-	py::scoped_interpreter guard{};
-	py::exec(R"(
-		print("[py] Hello World!")
-	)");
-
+	try {
+		py::scoped_interpreter guard{};
+		py::exec(R"(
+			print("[py] Hello World!")
+		)");
+	}
+	catch (const std::exception& e) {
+		std::cout << e.what() << std::endl;
+	}
 }

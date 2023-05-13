455486bbd5aa0733f277f825ad87b1c44df7b02f
LiamSalass <liamsalass@yahoo.com>
2023-05-13 18:51:35 -0400

    Linking multiple files works

--- embedded_testing/main/test.cpp
+++ embedded_testing/main/test.cpp
@@ -7,15 +7,17 @@
 #include <pybind11/embed.h>
 
 namespace py = pybind11;
+using namespace std;
 
 int main(int argc, char* argv[])
 {
 	std::cout << "[cpp] Hello World!" << std::endl;
 	try {
 		py::scoped_interpreter guard{};
-		py::exec(R"(
-			print("[py] Hello World!")
-		)");
+		auto exampleModule = py::module::import("scripts.example");
+		auto func = exampleModule.attr("add");
+		int sum = func(3, 5).cast<int>();
+		cout << sum << endl;
 	}
 	catch (const std::exception& e) {
 		std::cout << e.what() << std::endl;

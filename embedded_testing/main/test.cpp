b7566c4f36b0a23d233ee475b97401e82d902872
LiamSalass <liamsalass@yahoo.com>
2023-05-15 13:38:23 -0400

    Testing

--- embedded_testing/main/test.cpp
+++ embedded_testing/main/test.cpp
@@ -13,12 +13,20 @@ int main(int argc, char* argv[])
 {
 	try {
 		py::scoped_interpreter guard{};
+
+		//Testing example file
+		auto example = py::module::import("scripts.example");
+		auto func = example.attr("add");
+		int sum = func(1, 2).cast<int>();
+		cout << "sum: " << sum << endl;
+
+		//Testing radius file
 		auto radius3d_lm = py::module::import("scripts.3DRadius_lm");
-		auto exe_radiuslm = radius3d_lm.attr("exe_radiuslm");
-		exe_radiuslm();
-		cout << "Test completed" << endl;
+		//auto exe_radiuslm = radius3d_lm.attr("exe_radiuslm");
+		//exe_radiuslm();
+		cout << "Test end" << endl;
 	}
-	catch (const std::exception& e) {
+	catch (py::error_already_set& e) {
 		std::cout << e.what() << std::endl;
 	}
 }

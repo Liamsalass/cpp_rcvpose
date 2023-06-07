586d6c8db1b3e107148effa84efd030e17d46917
Liam Salass <91794824+Liamsalass@users.noreply.github.com>
2023-06-06 23:03:13 -0400

    Delete rcvpose/lmshorn directory

--- rcvpose/lmshorn/JACOBI.h
+++
@@ -1,1 +0,0 @@
-void myjacobi(double a[5][5], int n, double d[5], double v[5][5], int *nrot) ;
--- rcvpose/lmshorn/lmshorn.h
+++
@@ -1,3 +0,0 @@
-//	extern "C" {
-void lmshorn(double (*P1)[3], double (*P2)[3], int n, double A[4][4]) ;
-//	}	

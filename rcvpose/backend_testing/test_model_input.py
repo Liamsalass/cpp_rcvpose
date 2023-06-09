1f2b51de026ad37773c46aaeb5b2bc261d2f0759
LiamSalass <liamsalass@yahoo.com>
2023-06-09 17:10:31 -0400

    Fixing save functionality

--- rcvpose/backend_testing/test_model_input.py
+++ rcvpose/backend_testing/test_model_input.py
@@ -1,29 +1,20 @@
-import torch
+import torch as to
+from torch import optim
+from res_net import DenseFCNResNet152
 
-folder_path = r'C:\Users\User\.cw\work\cpp_rcvpose\rcvpose\out\build\x64-debug\train_kpt1\model_best'
+class ModelLoader:
+    def __init__(self, path):
+        input_archive = to.serialization.load(path + "/model.pt")
+        print(input_archive)
+        self.model = DenseFCNResNet152(3,2)
+        self.model = input_archive.load(path + "/model.pt")
+        print(self.model)
 
-# Load the model information
-model_info = torch.jit.load(folder_path + '/info.pt')
+        
 
-# Extract the relevant information
-epoch = model_info.epoch
-iteration = model_info.iteration
-model_arch = model_info.arch
-best_acc_mean = model_info.best_acc_mean
-val_loss = model_info.loss
-optimizer = model_info.optimizer
-current_lr = model_info.lr
+def main():
+    path = "C:/Users/User/.cw/work/cpp_rcvpose/rcvpose/out/build/x64-debug/train_kpt1/current"
+    loader = ModelLoader(path)
 
-# Load the model itself
-model = torch.nn.ModuleDict(folder_path + '/model.pt')
-
-# Set the model to evaluation mode
-#model.eval()
-
-print("epoch, ", epoch)
-print("iteration, ", iteration)
-print("model_arch, ", model_arch)
-print("best_acc_mean, ", best_acc_mean)
-print("val_loss, ", val_loss)
-print("optimizer, ", optimizer)
-print("current_lr, ", current_lr)
+if __name__ == "__main__":
+    main()

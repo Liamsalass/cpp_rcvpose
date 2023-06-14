57cbda55058a1a04f8a000c463b668db343a2d5a
LiamSalass <liamsalass@yahoo.com>
2023-06-14 13:45:21 -0400

    test storage

---
+++ test_storage/test_storage.cpp
@@ -0,0 +1,16 @@
+﻿// test_storage.cpp : Defines the entry point for the application.
+//
+
+#include "test_storage.h"
+#include <torch/torch.h>
+using namespace std;
+
+int main()
+{
+	// Create random torch tensor
+	torch::Tensor tensor = torch::rand({ 2, 3 });
+	cout << tensor << endl;
+	//store the tensor data to specified path
+	torch::save(tensor, "../../../../tensor.pt");
+
+}

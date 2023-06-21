d5a79d8c80d99374ffc00ac66c633f1d6ad7fa98
Liamsalass <liamsalass@yahoo.com>
2023-06-21 00:27:14 -0400

    Updating main and fixing building

--- rcvpose/test.cpp
+++ rcvpose/test.cpp
@@ -71,9 +71,16 @@ Options gpu_train_opts(int kpt, bool rsm, int batch_size, int gpuid) {
 
 int main(int argc, char* args[])
 {
-	Options opts = testing_options(1, false, 2, "kpt1");
-	RCVpose rcv(opts);
-	rcv.train();
+	Options opts1 = gpu_train_opts(1, true, 24, "kpt1", 0);
+	RCVpose rcv1(opts1);
 	rcv.save_tensor("kpt1_t", 0, 25);
 
+    Options opts2 = gpu_train_opts(2, true, 24, "kpt2", 0);
+    RCVpose rcv2(opts2);
+    rcv.save_tensor("kpt2_t", 0, 25);
+
+    Options opts3 = gpu_train_opts(3, true, 24, "kpt3", 0);
+    RCVpose rcv3(opts3);
+    rcv.save_tensor("kpt3_t", 0, 25);
+    
 }
--- rcvpose/trainer.h
+++ rcvpose/trainer.h
@@ -29,11 +29,8 @@ public:
 	void test_compute_r_loss();
 	
 	void store_model(std::string path);
-	void output_pred(const int& idx, const std::string& path);
 
 	void output_pred(const int& idx, const std::string& path);
-
-	void tensorToFile(const torch::Tensor& tensor, const std::string& path);
 
 private:
 	// Have to nest train_epoch function within train function due to instantiation of dataloaders

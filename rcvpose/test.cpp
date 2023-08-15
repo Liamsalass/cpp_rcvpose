bc3086491eb6006265361fc1e6bdd4545c3c9692
Liamsalass <liamsalass@yahoo.com>
2023-08-15 10:49:04 -0400

    basic changes to run on gpu

--- rcvpose/test.cpp
+++ rcvpose/test.cpp
@@ -11,20 +11,20 @@ Options testing_options() {
     Options opts;
     opts.gpu_id = 0;
     opts.dname = "lm";
-    opts.root_dataset = "C:/Users/User/.cw/work/datasets/test";
+    opts.root_dataset = "/ingenuity_NAS/dataset/public/RCVLab/Bluewrist/16yw113/";
     //or ".../dataset/public/RCVLab/Bluewrist/16yw11"
-    opts.model_dir = "C:/Users/User/.cw/work/cpp_rcvpose/rcvpose/test_out/ape/";
-    opts.resume_train = true;
+    opts.model_dir = "/models/";
+    opts.resume_train = false;
     opts.optim = "adam";
-    opts.batch_size = 2;
+    opts.batch_size = 18;
     opts.class_name = "ape";
     opts.initial_lr = 0.0001;
     opts.reduce_on_plateau = false;
     opts.patience = 10;
     opts.demo_mode = false;
-    opts.verbose = true;
+    opts.verbose = false;
     opts.test_occ = false;
-    return opts;
+    return opts; 
 }
 
 int main(int argc, char* args[])
